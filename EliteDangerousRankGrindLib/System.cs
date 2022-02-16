d2dbb5696c2c4d5671f98dd182d7ebaa153aea92
Ryan M <ryanpmcn@gmail.com>
2022-02-16 12:54:35 -0500

    Created static Filter_Json_File function. Filters data to useful information

--- EliteDangerousRankGrindLib/System.cs
+++ EliteDangerousRankGrindLib/System.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace EliteDangerousRankGrindLib
 {
@@ -23,5 +27,74 @@ namespace EliteDangerousRankGrindLib
         public DateTime updated_at { get; set; }
         public DateTime minor_factions_updated_at { get; set; }
         public IList<MinorFactions> minor_faction_presences { get; set; }
+
+
+        /// <summary>
+        /// Creates a default Json Serializer for filtering a given Json file
+        /// </summary>
+        /// <param name="systemFilePath"></param>
+        static void Filter_Json_File(String systemFilePath)
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            //writer.Write(JsonConvert.SerializeObject(systems));
+            serializer.Formatting = Newtonsoft.Json.Formatting.Indented;
+            serializer.NullValueHandling = NullValueHandling.Include;
+            Filter_Json_File(systemFilePath, serializer);
+        }
+
+        /// <summary>
+        /// Filters a given Json File.
+        /// Removes systems which require a permit and has three or less minor factions, these systems are not useful for calcuations
+        /// Removes data which is unnecessary for calcuations
+        /// Alliance and Indepentant systems are not removed as the factions in the system are counted, this data is in the factions Json file
+        /// </summary>
+        /// <param name="systemFilePath"></param>
+        /// <param name="serializer"></param>
+        static void Filter_Json_File(String systemFilePath, JsonSerializer serializer)
+        {
+            JArray preFilterSystems;
+
+            using (StreamReader reader = new StreamReader(systemFilePath))
+            {
+                preFilterSystems = JArray.Parse(reader.ReadToEnd());
+            }
+
+            using (StreamWriter writer = new StreamWriter(systemFilePath))
+            {
+                var systems =
+                    from system in preFilterSystems
+                    where (bool)system["needs_permit"] == false && system["minor_faction_presences"].Count() > 3
+                    orderby system["allegiance"]
+                    select new
+                    {
+                        id = system["id"],
+                        edsm_id = system["edsm_id"],
+                        name = system["name"],
+                        x = system["x"],
+                        y = system["y"],
+                        z = system["z"],
+                        population = system["population"],
+                        government_id = system["government_id"],
+                        government = system["government"],
+                        allegiance_id = system["allegiance_id"],
+                        allegiance = system["allegiance"],
+                        security_id = system["security_id"],
+                        security = system["security"],
+                        primary_economy_id = system["primary_economy_id"],
+                        primary_economy = system["primary_economy"],
+                        updated_at = system["updated_at"],
+                        minor_factions_updated_at = system["minor_factions_updated_at"],
+                        minor_faction_presences = from presences in system["minor_faction_presences"]
+                                                  select new
+                                                  {
+                                                      minor_faction_id = presences["minor_faction_id"],
+                                                      influence = presences["influence"],
+                                                      active_states = presences["active_states"],
+                                                  }
+                    };
+
+                serializer.Serialize(writer, systems);
+            }
+        }
     }
 }
