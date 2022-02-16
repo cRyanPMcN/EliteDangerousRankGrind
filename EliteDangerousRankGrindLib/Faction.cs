78e8add64e1e74434ed958c59deb0bd7635b18de
Ryan M <ryanpmcn@gmail.com>
2022-02-16 13:03:27 -0500

    Created Faction.cs for factions Json file

---
+++ EliteDangerousRankGrindLib/Faction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EliteDangerousRankGrindLib
+{
+    public class Faction
+    {
+        public UInt64 id;
+        public String name;
+        public UInt16 government_id;
+        public String government;
+        public UInt16 allegiance_id;
+        public String allegiance;
+    }
+}
